272543e3b83c8601f7a17e53c04d1fe7852b7450
aerin <admin@alexerin.ru>
2024-10-25 18:55:16 +0300

    Сохранение/чтение настроек, удалены тесты, добавлено логирование

--- app/src/main/java/ru/alexerin/ledarrbt/MainActivity.kt
+++ app/src/main/java/ru/alexerin/ledarrbt/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.alexerin.ledarrbt
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -11,21 +10,36 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
+// Настройки приложения:
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore( name = "user_settings" )
 
 class MainActivity : AppCompatActivity()
 {
     // Контролы:
     private lateinit var btnFind: ImageButton
     private lateinit var btnConn: ImageButton
+    private lateinit var tb: Toolbar
 
     // Для показа списка связанных bt-устройств;
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
@@ -33,6 +47,10 @@ class MainActivity : AppCompatActivity()
     // Bluetooth:
     private lateinit var bluetoothManager: BluetoothManager
     private var bluetoothAdapter: BluetoothAdapter? = null
+
+    // Устройство bt:
+    private var btDeviceName: String = ""
+    private var btDeviceMac: String = ""
 
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?)
@@ -51,11 +69,18 @@ class MainActivity : AppCompatActivity()
         // Регистрируем обработчик выбора устройства из списка связанных устройств:
         resultLauncher = registerForActivityResult( ActivityResultContracts.StartActivityForResult())
         { result ->
-            if( Activity.RESULT_OK == result.getResultCode() )
+            if( RESULT_OK == result.getResultCode() )
             {
                 val intent = result.data
-                val name = intent!!.getStringExtra("name" )
-                Toast.makeText(this, name, Toast.LENGTH_SHORT ).show()
+                if (intent != null)
+                {
+                    btDeviceName = intent.getStringExtra("name" ).toString()
+                    cfgSave( "device_name", btDeviceName )
+                    btDeviceMac = intent.getStringExtra("mac" ).toString()
+                    cfgSave( "device_mac", btDeviceMac )
+
+                    tb.title = btDeviceName
+                }
             }
             else
             {
@@ -64,8 +89,14 @@ class MainActivity : AppCompatActivity()
             }
         }
 
-        btnFind = findViewById<ImageButton>( R.id.btnFind )
-        btnConn = findViewById<ImageButton>( R.id.btnConnect )
+        btnFind = findViewById<ImageButton>( R.id.btnFind ) as ImageButton
+        btnConn = findViewById<ImageButton>( R.id.btnConnect ) as ImageButton
+        tb = findViewById<View>(R.id.toolbar) as Toolbar
+
+        btDeviceName = cfgLoad( "device_name" )
+        btDeviceMac = cfgLoad( "device_mac" )
+        Log.d("DataStore", "Saved device: " + btDeviceName + " [" + btDeviceMac + "]" )
+        tb.title = btDeviceName
 
         bluetoothManager = getSystemService( BluetoothManager::class.java )
         bluetoothAdapter = bluetoothManager.getAdapter()
@@ -143,5 +174,30 @@ class MainActivity : AppCompatActivity()
         btnConn.setBackgroundColor( Color.RED )
         btnFind.setOnClickListener { Toast.makeText(this, "Включите BT!", Toast.LENGTH_SHORT ).show() }
         btnConn.setOnClickListener { Toast.makeText(this, "Включите BT!", Toast.LENGTH_SHORT ).show() }
+    }
+
+    //-------------------------------------------------------------------------
+    // Сохранение value в качестве значения ключа key из DataStore:
+    //-------------------------------------------------------------------------
+    private fun cfgSave( key: String, value: String )
+    {
+        runBlocking {
+            this@MainActivity.dataStore.edit {
+                it[stringPreferencesKey( key )] = value
+            }
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    // Загрузка значения ключа key из DataStore:
+    //-------------------------------------------------------------------------
+    private fun cfgLoad( key: String ): String
+    {
+        return runBlocking {
+            val valueFlow: Flow<String> = this@MainActivity.dataStore.data.map {
+                it[stringPreferencesKey( key )] ?: ""
+            }
+            valueFlow.first()
+        }
     }
 }
